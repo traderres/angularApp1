5bc37eb9bca59a85dd5490f43bb99652210b4ed3
Adam Resnick <aresnick@rbr-tech.com>
2021-11-30 16:11:18 -0500

    Added custom validator to ensure that atmost 3 checkboxes are checked.

--- frontend/src/app/reports/add-report-custom-validator/add-report-custom-validator.component.ts
+++ frontend/src/app/reports/add-report-custom-validator/add-report-custom-validator.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, ValidatorFn} from "@angular/forms";
 
 @Component({
   selector: 'app-add-report-custom-validator',
@@ -12,19 +12,28 @@ export class AddReportCustomValidatorComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder) { }
 
+
   public ngOnInit(): void {
-    // Initialize the form
+
+    // Initialize the form and set a form validator
     this.myForm = this.formBuilder.group({
-      report_name: [null, null],
-      chk1: [null, null],
-      chk2: [null, null],
-      chk3: [null, null],
-      chk4: [null, null],
-      chk5: [null, null],
-    });
+        report_name: [null, null],
+        chk1: [false, null],
+        chk2: [false, null],
+        chk3: [false, null],
+        chk4: [false, null],
+        chk5: [false, null],
+      },
+      {
+        validators: this.validateMaxCheckboxes(3)
+      });
 
   }  // end of ngOnInit()
 
+
+  /*
+   * User pressed the Submit button
+   */
   public submitPressed(): void {
     // Touch all forms (so validation messages appear)
     this.myForm.markAllAsTouched();
@@ -38,7 +47,65 @@ export class AddReportCustomValidatorComponent implements OnInit {
     this.myForm.reset();
   }
 
+  /*
+   * User pressed the Reset button
+   */
   public resetPressed(): void {
     this.myForm.reset();
   }
+
+
+  /*
+   * This is the custom validator
+   *
+   * It returns null if there are no errors
+   * It returns a map that holds a key and value=error message displayed on the page
+   */
+  private validateMaxCheckboxes(aMaxCheckboxesChecked: number): ValidatorFn {
+    return () => {
+
+      const totalChecked = this.getTotalCheckboxesChecked();
+      if (totalChecked < aMaxCheckboxesChecked) {
+        // The user has not selected any values -- so assume everything is valid
+        return null;
+      }
+      else {
+        // The user has checked too many checkboxes.  So return a validation error
+        return {'validateMaxCheckboxes': `You must select at most ${aMaxCheckboxesChecked} checkboxes.`};
+      }
+
+    }
+  }  // end of validateMaxCheckboxes()
+
+
+  /*
+   * This method returns the total number of checkboxes that are checked
+   */
+  private getTotalCheckboxesChecked(): number {
+    if (! this.myForm) {
+      // Ths form is not fully initialized.  So, return zero.
+      return 0;
+    }
+
+    let totalChecked: number = 0;
+
+    if (this.myForm.controls.chk1.value) {
+      totalChecked++;
+    }
+    if (this.myForm.controls.chk2.value) {
+      totalChecked++;
+    }
+    if (this.myForm.controls.chk3.value) {
+      totalChecked++;
+    }
+    if (this.myForm.controls.chk4.value) {
+      totalChecked++;
+    }
+    if (this.myForm.controls.chk5.value) {
+      totalChecked++;
+    }
+
+    return totalChecked;
+  }
+
 }
